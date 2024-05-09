c6ac709906f2b7f74519ef21365f964f4d0697b2
valentino <126835108+Valen89hh@users.noreply.github.com>
2024-05-08 22:00:31 -0500

    add typography

--- app/src/main/java/com/example/qrshare/MainActivity.kt
+++ app/src/main/java/com/example/qrshare/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.qrshare.ui.theme.QrShareTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,7 +25,7 @@ class MainActivity : ComponentActivity() {
             QrShareTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Daniel",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -36,7 +38,9 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        color = Color.Red,
+        fontStyle = MaterialTheme.typography.titleLarge.fontStyle
     )
 }
 
--- app/src/main/java/com/example/qrshare/ui/theme/Type.kt
+++ app/src/main/java/com/example/qrshare/ui/theme/Type.kt
@@ -2,27 +2,33 @@ package com.example.qrshare.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.qrshare.R
 
+val Rubik = FontFamily(
+    Font(R.font.rubik)
+)
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Rubik,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
+    titleLarge = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.Normal,
+    fontSize = 22.sp,
+    lineHeight = 28.sp,
+    letterSpacing = 0.sp
+    ),
+    /* Other default text styles to override
+
     labelSmall = TextStyle(
         fontFamily = FontFamily.Default,
         fontWeight = FontWeight.Medium,
